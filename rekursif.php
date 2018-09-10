7338a2e1a256e6911f5c4f5448bbf5ea958201ea
fadilarmnado <42725259+fadilarmnado@users.noreply.github.com>
2018-09-10 21:17:42 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,34 @@
 <?php
+function piramida1($i,$j){
+  if($i > 0){
+  	if ($j > 0) {
+  		echo "*";
+  		$j--;
+  		piramida1($i,$j);
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+  	}else{
+  		echo "<br>";
+  		$i--;
+  		piramida1($i,$i);
+  	}
+  }  
 }
-rekursif(5);
+piramida1(5,5);
+echo "<br>*=========================================*<br>";
+$angka = 5;
+function piramida2($i,$j,$angka){
+	if ($i <= $angka) {
+		if ($j <= $i) {
+			echo $i;
+			$j++;
+			piramida2($i,$j,$angka);
+		}else{
+			echo "<br>";
+			$i++;
+			piramida2($i,1,$angka);
+		}
+	}
+}
+piramida2(1,1,$angka);
+
 ?>
